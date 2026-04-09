78c320a2ef2a85e957bc5db25d8b813a03ea7b6e
Anthonny Almeida <anthonyjose100k@gmail.com>
2026-04-09 14:49:03 -0300

    Alteração no JavaScript
    Adicionei a função corretamente, estava escrita errado

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 //VARIÁVEIS 
-const email = getElementById("EmailInput");
-const senha = getElementById("SenhaInput");
+const email = document.getElementById("EmailInput");
+const senha = document.getElementById("SenhaInput");
 
 //TESTE DE DADOS MOCKADOS
 const USER_MOCK  = "teste@email.com";
 const SENHA_MOCK = "123456";
 
 //FUNÇÃO QUE MOSTRAR O FUNCIONAMENTO DA IDEIA
-function fazerLogin() {
+function fazerLogin()  {
   if (email.value === USER_MOCK && senha.value === SENHA_MOCK) {
     window.location.href = "home.html"
   } else{
      alert("Email ou senha incorretos!!");
   }
-}
-function click(){
-    
-    
 }
