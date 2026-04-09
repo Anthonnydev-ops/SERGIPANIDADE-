da27720a3937a30e44b9a492c4113e15025cdadb
Anthonny Almeida <anthonyjose100k@gmail.com>
2026-04-09 14:19:43 -0300

    HTML CSS E JS
    Ai estao os arquivos que vamos utilizar para testar os dados com JS puro, depois usaremos REACT

---
+++ script.js
@@ -0,0 +1,20 @@
+//VARIÁVEIS 
+const email = getElementById("EmailInput");
+const senha = getElementById("SenhaInput");
+
+//TESTE DE DADOS MOCKADOS
+const USER_MOCK  = "teste@email.com";
+const SENHA_MOCK = "123456";
+
+//FUNÇÃO QUE MOSTRAR O FUNCIONAMENTO DA IDEIA
+function fazerLogin() {
+  if (email.value === USER_MOCK && senha.value === SENHA_MOCK) {
+    window.location.href = "home.html"
+  } else{
+     alert("Email ou senha incorretos!!");
+  }
+}
+function click(){
+    
+    
+}
